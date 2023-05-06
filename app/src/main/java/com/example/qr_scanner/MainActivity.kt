98620b90bb99526cdb3ac18d0e9564a2796c198e
IT21377426 <it21377426@my.sliit.lk>
2023-05-07 00:17:36 +0530

    12:17AM 5/7/2023

---
+++ app/src/main/java/com/example/qr_scanner/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.qr_scanner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, Roles::class.java))
+            finish()
+        },1000)
+
+    }
+}
